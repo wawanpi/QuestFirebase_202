eecee54bacf0f817d32a4bbd872bbbf872285589
wawanpi <muhaguskurniawan@gmail.com>
2025-01-12 17:53:09 +0700

    menambahkan fungsi delete

--- app/src/main/java/com/example/pertemuan14/ui/ViewModel/HomeViewModel.kt
+++ app/src/main/java/com/example/pertemuan14/ui/ViewModel/HomeViewModel.kt
@@ -47,4 +47,13 @@ class HomeViewModel (
                 }
         }
     }
+    fun deleteMhs(mahasiswa: Mahasiswa) {
+        viewModelScope.launch {
+            try {
+                mhs.deleteMahasiswa(mahasiswa)
+            }catch (e: Exception) {
+                mhsUIState = HomeUiState.Error(e)
+            }
+        }
+    }
 }
