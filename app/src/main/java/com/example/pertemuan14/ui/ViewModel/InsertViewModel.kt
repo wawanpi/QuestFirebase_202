25a781425ad706cc8487bf35262ee213f2752532
wawanpi <muhaguskurniawan@gmail.com>
2025-01-12 18:07:54 +0700

    membuat data class MahasiswaEvent

--- app/src/main/java/com/example/pertemuan14/ui/ViewModel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan14/ui/ViewModel/InsertViewModel.kt
@@ -3,6 +3,15 @@ package com.example.pertemuan14.ui.ViewModel
 import com.example.pertemuan14.model.Mahasiswa
 
 
+
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = "",
+)
 fun MahasiswaEvent.toMhsModel() : Mahasiswa = Mahasiswa(
     nim = nim,
     nama = nama,
