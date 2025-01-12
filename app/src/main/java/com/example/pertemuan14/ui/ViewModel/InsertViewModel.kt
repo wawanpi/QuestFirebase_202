e342d666a9a843fbacde4b6835cee877c42a2ab9
wawanpi <muhaguskurniawan@gmail.com>
2025-01-12 18:08:28 +0700

    membuat class FromErrorState

--- app/src/main/java/com/example/pertemuan14/ui/ViewModel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan14/ui/ViewModel/InsertViewModel.kt
@@ -4,6 +4,19 @@ import com.example.pertemuan14.model.Mahasiswa
 
 
 
+data class FromErrorState(
+    val nim: String?= null,
+    val nama: String?= null,
+    val jenisKelamin: String?= null,
+    val alamat: String?= null,
+    val kelas: String?= null,
+    val angkatan: String?= null,
+    ){
+    fun isValid(): Boolean {
+        return nim == null && nama == null && jenisKelamin == null && alamat == null && kelas == null && angkatan == null
+    }
+}
+
 data class MahasiswaEvent(
     val nim: String = "",
     val nama: String = "",
